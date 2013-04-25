ac7faed6a64ceef2adfc4ff7467fd90374c00e62
Ben Lavender <bhuga@github.com>
2013-04-25 15:31:10 -0500

    Wrap pjax setup so spork can trap it

--- lib/pjax_rails.rb
+++ lib/pjax_rails.rb
@@ -3,7 +3,7 @@ require 'pjax'
 module PjaxRails
   class Engine < ::Rails::Engine
     initializer "pjax_rails.add_controller" do
-      config.to_prepare { ApplicationController.send :include, Pjax }
+      config.to_prepare { Pjax.setup }
     end
   end
 end
